922b67b7a41df0787a3f29f334f9efa05c740419
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2020-05-04 16:09:27 +0900

    Added acc_load test

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function check_args(split, m_sec){
+function check_args(split, msec){
   if(isNaN(split)){
     const msg = "invalid split" 
     throw msg  
   }
 
-  if(isNaN(m_sec)){
-    const msg = "invalid m_sec" 
+  if(isNaN(msec)){
+    const msg = "invalid msec" 
     throw msg  
   }
 
@@ -25,13 +25,11 @@ function rest_array(destination_arr){
   destination_arr.splice(0, destination_arr.length)
 }
 
-const load = async function(destination_arr, source_arr, split, m_sec){
+// if call acc_load in load method then npm test failed
+// so, Not calling acc_load
+// perhaps The cause is async
+const load = async function(destination_arr, source_arr, split, msec){
   rest_array(destination_arr)
-  acc_load(destination_arr, source_arr, split, m_sec)
-}
-
-const acc_load = async function(destination_arr, source_arr, split, m_sec){
-  check_args(split, m_sec)
   let index = 0;
   while(index < source_arr.length){
     let splited_arr = source_arr.slice(index, index + split)
@@ -39,7 +37,20 @@ const acc_load = async function(destination_arr, source_arr, split, m_sec){
     // vue don't detect concat method
     // so, instead of concat use push 
     for(let i = 0; i < splited_arr.length; i++){ destination_arr.push(splited_arr[i]) }
-    await sleep(m_sec)
+    await sleep(msec)
+  }
+}
+
+const acc_load = async function(destination_arr, source_arr, split, msec){
+  check_args(split, msec)
+  let index = 0;
+  while(index < source_arr.length){
+    let splited_arr = source_arr.slice(index, index + split)
+    index += split
+    // vue don't detect concat method
+    // so, instead of concat use push 
+    for(let i = 0; i < splited_arr.length; i++){ destination_arr.push(splited_arr[i]) }
+    await sleep(msec)
   }
 }
 
--- test/test-index.js
+++ test/test-index.js
@@ -1,46 +1,106 @@
 const test = require('ava');
 const thunderGirl = require("../index")
 
-test('should copy source_arr to destination_arr', async t => {
+test('should load source_arr to destination_arr', async t => {
   let destination_arr = []
   let source_arr = [1, 2, 3]
 
   await thunderGirl.load(destination_arr, source_arr, 1, 0);
-  t.is(destination_arr.length, source_arr.length);
-  t.is(destination_arr[0], source_arr[0]);
-  t.is(destination_arr[1], source_arr[1]);
-  t.is(destination_arr[2], source_arr[2]);
+  t.is(3, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
 });
 
-test('should copy source_arr to destination_arr by 2 split', async t => {
+test('should load source_arr to destination_arr by 2 split', async t => {
   let destination_arr = []
   let source_arr = [1, 2, 3]
 
   await thunderGirl.load(destination_arr, source_arr, 2, 0)
-  t.is(destination_arr.length, source_arr.length);
-  t.is(destination_arr[0], source_arr[0]);
-  t.is(destination_arr[1], source_arr[1]);
-  t.is(destination_arr[2], source_arr[2]);
+  t.is(3, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
 });
 
-test('should copy source_arr to destination_arr by 3 split', async t => {
+test('should load source_arr to destination_arr by 3 split', async t => {
   let destination_arr = []
   let source_arr = [1, 2, 3]
 
   await thunderGirl.load(destination_arr, source_arr, 3, 0)
-  t.is(destination_arr.length, source_arr.length);
-  t.is(destination_arr[0], source_arr[0]);
-  t.is(destination_arr[1], source_arr[1]);
-  t.is(destination_arr[2], source_arr[2]);
+  t.is(3, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
 });
 
-test('should copy source_arr to destination_arr case of 1000sec', async t => {
+test('should load source_arr to destination_arr case of 1000msec', async t => {
   let destination_arr = []
   let source_arr = [1, 2, 3]
 
   await thunderGirl.load(destination_arr, source_arr, 1, 1000)
-  t.is(destination_arr.length, source_arr.length);
-  t.is(destination_arr[0], source_arr[0]);
-  t.is(destination_arr[1], source_arr[1]);
-  t.is(destination_arr[2], source_arr[2]);
+  t.is(3, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
+});
+
+test('should load source_arr to destination_arr two times', async t => {
+  let destination_arr = []
+  let source_arr = [1, 2, 3]
+
+  await thunderGirl.load(destination_arr, source_arr, 3, 0)
+  t.is(3, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
+
+  await thunderGirl.load(destination_arr, source_arr, 3, 0)
+  t.is(3, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
+});
+
+test('should load source_arr to destination_arr in the case of split is bigger than length', async t => {
+  let destination_arr = []
+  let source_arr = [1, 2, 3]
+
+  await thunderGirl.load(destination_arr, source_arr, 100, 0)
+  t.is(3, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
+});
+
+test('should accumulately load source_arr to destination_arr', async t => {
+  let destination_arr = []
+  let source_arr = [1, 2, 3]
+
+  await thunderGirl.acc_load(destination_arr, source_arr, 3, 0)
+  t.is(3, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
+
+  await thunderGirl.acc_load(destination_arr, source_arr, 3, 0)
+  t.is(6, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
+
+  await thunderGirl.acc_load(destination_arr, source_arr, 3, 0)
+  t.is(9, destination_arr.length);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
+  t.is(1, destination_arr[0]);
+  t.is(2, destination_arr[1]);
+  t.is(3, destination_arr[2]);
 });
